a9333cd07a5f96d66df7a5ce6e48ecfc8a711e39
SUPERGAMESGROUP <SUPERGAMESGROUP@protonmail.com>
2019-03-07 00:00:04 +0300

    + GUI +AnimationProject

---
+++ AnimationProject/Assets/Anim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Anim : MonoBehaviour
+{
+    Animator m_animator;//создаем экземпляр класса Animator
+
+    void Start()
+    {
+        m_animator = gameObject.GetComponent<Animator>();//присваиваем экземпляру класса компонент Animator (который находится в кубе)
+
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {//при нажатии на клавишу мыши
+            m_animator.SetBool("Go", true); //параметр Go меняется на true
+
+        }
+    }
+}
